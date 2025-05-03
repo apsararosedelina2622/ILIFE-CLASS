bed3a4416002e74b45b942833110ba3554f6ca40
apsararosedelina2622 <apsararosedelina@gmail.com>
2025-05-03 17:48:57 +0530

    commit

--- REACT/src/Day13.jsx
+++ REACT/src/Day13.jsx
@@ -1,89 +1,48 @@
-// import React, { useState } from 'react'
-
-// const Day13 = () => {
-
-//     const [input, setInput] = useState('')
-//     const [list, setList] = useState([])
-//     const [editText, setEditText] = useState('')
-//     const [edit, setEdit] = useState(null)
-
-//     const Add = () => {
-//         setList(prev => [...prev, input])
-//         setInput('')
-//     }
-
-//     const Delete = (index) => {
-//         if(window.confirm('Are You Sure Want To Delete?')){
-//             setList(list.filter((a,i) => i !== index))
-//         }
-//     }
-
-//     const Edit = (index) => {
-//         setEdit(index)
-//         setEditText(list[index])
-//     }
-
-//     const Update = () => {
-//         const update = list.map((value,i) => (i === edit ? editText : value))
-//         setList(update);
-//         setEdit(null)
-//         setEditText('')
-//     }
-
-//   return (
-//     <div>
-//         <div className="container">
-//             <input type="text" className='form-control' value={input} onChange={e => setInput(e.target.value)} />
-//             <button onClick={Add}>Add</button>
-//         </div>
-//         <p>{list.map((value,index) => {
-//             return(
-//                 <table>
-//                     <tr>
-//                         {edit === index ? (
-//                             <input type='text' value={editText} onChange={e => setEditText(e.target.value)} />
-//                         ) : (
-//                             <td>{value}</td>
-//                         )}
-//                         <td>
-//                             {edit === index ? (
-//                                 <button onClick={Update}>Update</button>
-//                             ) : (
-//                                 <button onClick={() => Edit(index)}>Edit</button>
-//                             )}
-//                         </td>
-//                         <td><button onClick={() => Delete(index)}>Delete</button></td>
-//                     </tr>
-//                 </table>
-//             )
-//         })}</p>
-
-//     </div>
-//   )
-// }
-
-// export default Day13
-
-
-
 import React, { useState } from 'react'
-import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
 
 const Day13 = () => {
 
     const [input , setInput] = useState("")
     const [list , setList] = useState([])
+    const [showModal , setShowModal] = useState(false)
+    const [viewList , setViewList] = useState("")
+    const [edit , setEdit] = useState("")
+    const [editText , setEditText] = useState("")
 
     const AddFun = () => {
-        setList(list.concat( input))
-        // setList([...list , input])
-        setInput("")
+        if(input.length === 0){
+            alert("Add list!")
+        }else{
+            setList(list.concat(input))
+            setInput("")
+        }
     }
 
-    const DeleteFun = (i) => {
-        if(window.confirm("Are you sure want to delete this?")){
-            setList(list.filter((value , index) => index !== i ))
+    const DeleteFun = (index) => {
+        if(window.confirm("Are you sure want to delete?")){
+            setList(list.filter((a , i) => i !== index))
         }
+    }
+
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
+    const ViewFun = (value) => {
+        setShowModal(true)
+        setViewList(value)
+    }
+
+    const EditFun = (index) => {
+        setEdit(index)
+        setEditText(list[index])
+    }
+
+    const UpdateFun = () => {
+        const updatedValue = list.map((value , index) => index === edit ? editText : value)
+        setList(updatedValue)
+        setEdit(null)
     }
 
   return (
@@ -91,32 +50,61 @@ const Day13 = () => {
         <div className="container p-5">
             <div className="row mb-5">
                 <div className="col-8">
-                    <input type="text" className='form-control' value={input} onChange={e => setInput(e.target.value)} />
+                    <input type="text" value={input} onChange={e => setInput(e.target.value)} className="form-control" />
                 </div>
                 <div className="col-4">
-                    <button onClick={AddFun} className='btn btn-primary w-100'>Add</button>
+                    <button className="btn btn-primary w-100" onClick={AddFun}>Add</button>
                 </div>
             </div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>List</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                {list.map((value , index) => {
-                    return(
-                        <tbody key={index}>
+
+            {list.length === 0 ?
+                (
+                    <p className='text-center'>Empty List!</p>
+                ) : (
+                    <table className='table table-bordered'>
+                        <thead>
                             <tr>
-                                <td>{value}</td>
-                                <td><button className='btn btn-warning w-100'>Edit</button></td>
-                                <td><button onClick={() => DeleteFun(index)} className='btn btn-danger w-100'>Delete</button></td>
+                                <th>List</th> 
+                                <th>Action</th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            {list.map((value , index) => {
+                                return(
+                                    <tr key={index}>
+                                        <td>
+                                            {edit === index ? (
+                                                <input type='text' value={editText} onChange={e => setEditText(e.target.value)} className='form-control' />
+                                            ) : (
+                                                value
+                                            )}
+                                        </td>
+                                        <td className='d-flex gap-3'>
+                                            <button className='btn btn-success w-100' onClick={() => ViewFun(value)}>View</button>
+                                            {edit === index ? (
+                                                <button className='btn btn-warning w-100' onClick={UpdateFun}>Update</button>
+                                            ) : (
+                                                <button className='btn btn-warning w-100' onClick={() => EditFun(index)}>Edit</button>
+                                            )}
+                                            <button className='btn btn-danger w-100' onClick={() => DeleteFun(index)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
-                    )
-                })}
-            </Table>
+                    </table>
+                )    
+            }
+
+            <Modal show={showModal}>
+                <Modal.Header closeButton onHide={handleClose}>
+                    <h4>Your Task</h4>
+                </Modal.Header>
+                <Modal.Body>
+                    <h5 className='text-center'>Task : {viewList}</h5>
+                </Modal.Body>
+            </Modal>
+            
         </div>
     </>
   )
