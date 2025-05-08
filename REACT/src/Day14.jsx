ce5060fa6d62e0ec872b70d3c9c14bd070df1ffb
apsararosedelina2622 <apsararosedelina@gmail.com>
2025-05-08 18:35:47 +0530

    commit

--- REACT/src/Day14.jsx
+++ REACT/src/Day14.jsx
@@ -1,21 +1,181 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+// Swiper 
 
-// Import Swiper styles
-import 'swiper/css';
+// import React from 'react'
+// import {Swiper, SwiperSlide} from "swiper/react"
 
-export default () => {
+// import "swiper/css"
+
+// const Day14 = () => {
+//   return (
+//     <Swiper
+//       slidesPerView={3}
+//     >
+//       <SwiperSlide>Slide 1</SwiperSlide>
+//       <SwiperSlide>Slide 2</SwiperSlide>
+//       <SwiperSlide>Slide 3</SwiperSlide>
+//       <SwiperSlide>Slide 4</SwiperSlide>
+//       <SwiperSlide>Slide 5</SwiperSlide>
+//     </Swiper>
+//   )
+// }
+
+// export default Day14
+
+
+// Swiper with Images
+
+// import React from 'react'
+// import {Swiper, SwiperSlide} from "swiper/react"
+
+// import "swiper/css"
+
+// const Day14 = () => {
+//   return (
+//     <Swiper
+//       slidesPerView={3}
+//       spaceBetween={20}
+//     >
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//     </Swiper>
+//   )
+// }
+
+// export default Day14
+
+
+// Swiper with Navigation
+
+// import React from 'react'
+// import {Swiper, SwiperSlide} from "swiper/react"
+// import {Navigation} from "swiper/modules"
+
+// import "swiper/css"
+// import "swiper/css/navigation"
+
+// const Day14 = () => {
+//   return (
+//     <Swiper
+//       slidesPerView={3}
+//       spaceBetween={20}
+//       modules={[Navigation]}
+//       navigation
+//     >
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//     </Swiper>
+//   )
+// }
+
+// export default Day14
+
+
+// Swiper with Navigation , Pagination
+
+// import React from 'react'
+// import {Swiper, SwiperSlide} from "swiper/react"
+// import {Navigation, Pagination} from "swiper/modules"
+
+// import "swiper/css"
+// import "swiper/css/navigation"
+// import "swiper/css/pagination"
+
+// const Day14 = () => {
+//   return (
+//     <Swiper
+//       slidesPerView={3}
+//       spaceBetween={20}
+//       modules={[Navigation , Pagination]}
+//       navigation
+//       pagination={{clickable: true}}
+//     >
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//       <SwiperSlide>
+//         <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+//       </SwiperSlide>
+//     </Swiper>
+//   )
+// }
+
+// export default Day14
+
+
+// Swiper with Navigation , Pagination , Autoplay
+
+import React from 'react'
+import {Swiper, SwiperSlide} from "swiper/react"
+import {Autoplay, Navigation, Pagination} from "swiper/modules"
+
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+const Day14 = () => {
   return (
     <Swiper
-      spaceBetween={50}
       slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+      spaceBetween={20}
+      modules={[Navigation , Pagination , Autoplay]}
+      navigation
+      pagination={{clickable : true}}
+      autoplay={{delay : 2000}}
+      speed={1000}
+      loop={true}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>
+        <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" height={300} className='w-100' />
+      </SwiperSlide>
     </Swiper>
-  );
-};
+  )
+}
+
+export default Day14
