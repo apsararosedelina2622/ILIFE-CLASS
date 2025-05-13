6fbebbd9f85e5a72aa55e830b2a8c87abcbef2c0
apsararosedelina2622 <apsararosedelina@gmail.com>
2025-05-13 19:00:20 +0530

    commit

---
+++ NODE_JS/DAY_1/export.js
@@ -0,0 +1,37 @@
+// Example - 1
+
+// var a = 20;
+
+// module.exports = a;
+
+
+// Example - 2
+
+// var a = 20;
+// var b = 30;
+
+// module.exports = { a , b }
+
+
+// Example - 3
+
+// var a = 20;
+// var b = 30;
+
+// module.exports = { a , b }
+
+
+// Example - 4
+
+// var Add = (a , b) => {
+//     return a + b
+// }
+
+// module.exports = Add
+
+
+// Example - 5
+
+// module.exports = (a , b) => {
+//     return a + b
+// }
---
+++ NODE_JS/DAY_1/import.js
@@ -0,0 +1,37 @@
+// Example - 1
+
+// var num = require("./export.js")
+
+// console.log(num)
+
+
+// Example - 2
+
+// var { a , b } = require("./export.js")
+
+// console.log(a)
+// console.log(b)
+
+
+// Example - 3
+
+// var data = require("./export.js")
+
+// console.log(data)
+
+// console.log(data.a)
+// console.log(data.b)
+
+
+// Example - 4
+
+// var Add = require("./export.js")
+
+// console.log(Add(5 , 10))
+
+
+// Example - 5
+
+// var Add = require("./export.js")
+
+// console.log(Add(5 , 10))
