2f5c1c5d182ce7db07fc0b6ceec3c99aa2f5663f
apsararosedelina2622 <apsararosedelina@gmail.com>
2025-07-19 17:46:50 +0530

    commit

---
+++ CLASS/app.js
@@ -0,0 +1,226 @@
+// function Fun(){
+//     alert("External JS")
+// }
+
+// VARIABLES
+
+// var , let , const 
+
+// var data = "Hello"
+// var data = "Hiii"
+// data = "Hiii"
+
+// let data = "Hello"
+// let data = "Hiii"
+// data = "Hiii"
+
+// const data = "Hello"
+// const data = "Hello"
+// data = "Hello"
+
+// console.log(data)
+
+
+// DATA TYPES
+
+// var data = "HELLO"
+// var data = "4567"
+
+// var data = 456
+
+// var data = true
+// var data = false
+
+// var data = ["HTML" , "CSS" , "BOOTSTRAP", "JS"]
+
+// var object = {
+//     key : value , 
+//     key : value
+// }
+
+// var data = {
+//     name : "Abi" , 
+//     age : 21 , 
+//     place : "Trichy"
+// }
+
+// var data = new Date()
+
+// var data = null
+
+// console.log(typeof(data))
+
+
+// var date = new Date()
+
+// console.log(date)
+// console.log(date.getDate())
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getDay())
+// console.log(date.toLocaleTimeString())
+
+
+// var data = ["HTML" , "CSS" , "BOOTSTRAP" , "JS"]
+
+// console.log(arrayName[position/index])
+// console.log(data)
+// console.log(data[0])
+// console.log(data[1])
+// console.log(data[2])
+// console.log(data[3])
+
+// var data = ["is" , "My" , "name" , "Abi"]
+
+// console.log(data[1])
+// console.log(data[2])
+// console.log(data[0])
+// console.log(data[3])
+
+// console.log(`Detail : ${data[1]} ${data[2]} ${data[0]} ${data[3]}`)
+// console.log("Detail : " , data[1] , data[2] , data[0] , data[3])
+// console.log("Detail : " + data[1] + data[2] + data[0] + data[3])
+
+// var data = {
+//     name : "Abi" , 
+//     age : 21 , 
+//     place : "Trichy" , 
+//     address : {
+//         doorno : 202 , 
+//         street : "3rd Street" , 
+//         city : "Chennai"
+//     }
+// }
+
+// console.log(objectName.key)
+// console.log(data)
+// console.log(data.name)
+// console.log(data.age)
+// console.log(data.place)
+// console.log(data.address)
+// console.log(data.address.doorno)
+// console.log(data.address.street)
+// console.log(data.address.city)
+
+// var data
+
+// console.log(data)
+
+// function HideFun(){
+//     document.getElementById("tag").innerHTML = ""
+// }
+
+// function ShowFun(){
+//     document.getElementById("tag").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laborum repellendus reprehenderit vitae provident voluptates deleniti saepe, nisi dolores odio aliquam doloribus. Ipsum, saepe quod unde nemo corrupti laboriosam quas."
+// }
+
+
+// var data = "Hello World!"
+// var data1 = "      Hello World!      "
+
+// console.log(data)
+// console.log(data.length)
+// console.log(data[4])
+// console.log(data.indexOf("H"))
+// console.log(data.includes("h"))
+// console.log(data.toUpperCase())
+// console.log(data.toLowerCase())
+// console.log(data.substr(s.p , count))
+// console.log(data.substr(0 , 6))
+// console.log(data.substr(6 , 6))
+// console.log(data.replace(old value , new value))
+// console.log(data.replace("World" , "JS"))
+
+// console.log(data)
+// console.log(data1)
+// console.log(data1.trim())
+
+// var num = "78"
+
+// console.log(num)
+// console.log(num.toFixed())
+
+// console.log(typeof(num))
+// console.log(num + num)
+// console.log(typeof(Number(num)))
+// console.log(Number(num) + Number(num))
+
+// var data = 89
+// var data = "Hello"
+// var data = "67"
+// var data = "89sdgsg"
+
+// console.log(isNaN(data))
+
+// var data = "45"
+// var data = "hfgdyfgd677"
+
+// console.log(isFinite(data))
+
+// var a = 10;
+// var b = 5;
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// var num = 7
+
+// num += 1
+// num -= 1
+
+// console.log(num)
+
+
+// console.log( "a" == "ab" )
+// console.log("as" === as)
+
+
+// let a = 89;
+// let b = 89;
+
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a <= b)
+
+
+// console.log((89 >= 89) || ("90" === 90)) // t || f = t
+// console.log(!((89 <= 87) || ("90" === 90))) // t || t = t
+
+// var num = 90
+
+// console.log(num)
+// console.log(num++)
+
+// var a = 10;
+
+// console.log(a++)
+// console.log(a)
+// console.log(++a)
+// console.log(a++)
+// console.log(++a)
+
+// var b = 25
+
+// console.log(b)
+// console.log(--b)
+// console.log(b--)
+// console.log(--b)
+// console.log(++b)
+// console.log(b)
+// console.log(b++)
+// console.log(--b)
+// console.log(b--)
+// console.log(b)
+
+
+// var data = ["HTML" , "CSS" , "BOOTSTRAP" , "JS"]
+
+// data.push("REACT")
+// data.pop()
+// data.shift()
+// data.unshift("HTML5")
+
+// console.log(data)
